3f96ca438e69862123721b0f4a896496644db12d
Khoa Nguyen <60150482+khoainho@users.noreply.github.com>
2026-04-29 10:37:34 -0500

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,9 @@
+import random
+
+class Solution:
+    def majorityElement(self, nums):
+        n = len(nums)
+        while True:
+            candidate = random.choice(nums)
+            if nums.count(candidate) > n // 2:
+                return candidate
